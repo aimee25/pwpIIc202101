f3e52326b775ebd57b4cd95d1d0c95240533dca8
aimee25 <aime25011994@gmail.com>
2021-04-02 21:02:54 -0600

    Proyecto en estado inicial funcionando

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', author: 'Aimee Hernandez Mendoza ', appName:'ejemplo s10' });  //se tiene    que reiniciar el servidor 
+  //appName en layout.hbs se tiene que crear primero 
+});
+/*agregando nueva ruta        http://localhost:3000/greeting    */
+router.get('/greeting', function(req, res, next){
+  res.send('HERNANDEZ MENDOZA VIVIANA AIMEE')
+  //* segundo ejemplo*/
+  //* res.status(200).json({message:'HERNANDEZ MENDOZA VIVIANA AIMEE'})   */
+})
+module.exports = router;
